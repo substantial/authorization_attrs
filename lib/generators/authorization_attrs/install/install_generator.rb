73638dc37ec7b95a505998650d00f1ae07bc9959
Derek Maffett <derek@substantial.com>
2015-07-19 01:07:13 -0700

    nest generator within module structure

--- lib/generators/authorization_attrs/install/install_generator.rb
+++ lib/generators/authorization_attrs/install/install_generator.rb
@@ -1,11 +1,15 @@
 require 'rails/generators'
 
-class SetupGenerator < Rails::Generators::Base
-  source_root File.expand_path("../templates", __FILE__)
+module AuthorizationAttrs
+  module Generators
+    class SetupGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
 
-  desc "Generates the appropriate model to use authorization_attrs"
+      desc "Generates the appropriate model to use authorization_attrs"
 
-  def setup_model
-    copy_file "authorization_attr.rb", "app/models/authorization_attr.rb"
+      def setup_model
+        copy_file "authorization_attr.rb", "app/models/authorization_attr.rb"
+      end
+    end
   end
 end
