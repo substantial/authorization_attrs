6c5c7dc6af359ec7274b3c46b647a2e9448fb1a5
Derek Maffett <derek@substantial.com>
2015-07-24 20:35:08 -0700

    remove spec_helper migration messages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,14 +9,17 @@ class AuthorizationAttr < ActiveRecord::Base
   belongs_to :authorizable, polymorphic: true
 end
 
-ActiveRecord::Migration.create_table "authorization_attrs" do |t|
-  t.string  :name
-  t.references :authorizable, polymorphic: true
+ActiveRecord::Migration.suppress_messages do
+  ActiveRecord::Migration.create_table "authorization_attrs" do |t|
+    t.string  :name
+    t.references :authorizable, polymorphic: true
+  end
+
+  ActiveRecord::Migration.add_index(
+    "authorization_attrs",
+    ["authorizable_type", "authorizable_id", "name"],
+    name: "index_on_authorizable_and_name",
+    unique: true
+  )
 end
 
-ActiveRecord::Migration.add_index(
-  "authorization_attrs",
-  ["authorizable_type", "authorizable_id", "name"],
-  name: "index_on_authorizable_and_name",
-  unique: true
-)
